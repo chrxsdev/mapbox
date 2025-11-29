ab5a3d12a682825e4fe1fa3f1dd82d5cf81b01ee
Cristhian Martinez <cristhian.martinezlara@gmail.com>
2025-11-29 17:01:34 -0600

    adding map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,16 +1,39 @@
-import { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useContext, useEffect, useRef } from 'react';
 import { Loading } from './Loading';
+import mapboxgl from 'mapbox-gl';
 
 import { PlacesContext } from '../context';
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isLoading && mapDiv.current) {
+      new mapboxgl.Map({
+        container: mapDiv.current!, // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: userLocation, // starting position [lng, lat]
+        zoom: 8, // starting zoom
+      });
+    }
+  }, [isLoading]);
 
   if (isLoading) return <Loading />;
 
   return (
-    <div>
-      <p>{userLocation?.join(', ')}</p>
-    </div>
+    <div
+      ref={mapDiv}
+      style={{
+        backgroundColor: 'red',
+        height: '100vh',
+        width: '100vw',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+      }}
+    ></div>
   );
 };
--- src/config/env.ts
+++ src/config/env.ts
@@ -6,12 +6,12 @@ interface Config {
 
 export const config: Config = {
   mapbox: {
-    token: import.meta.env.MAPBOX_ACCESS_TOKEN,
+    token: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
   },
 } as const;
 
 // Validate required environment variables
-const requiredEnvVars = [['MAPBOX_ACCESS_TOKEN', config.mapbox.token]] as const;
+const requiredEnvVars = [['VITE_MAPBOX_ACCESS_TOKEN', config.mapbox.token]] as const;
 
 for (const [name, value] of requiredEnvVars) {
   if (!value) {
