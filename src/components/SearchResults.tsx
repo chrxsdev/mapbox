5f952c8b9f976ce86790aeeb14fba74ba339efac
Cristhian Martinez <cristhian.martinezlara@gmail.com>
2025-11-29 20:30:45 -0600

    adding onPlaceClick to navigate to specific position

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,31 +1,49 @@
-import { useContext } from 'react';
-import { PlacesContext } from '../context';
+import { useContext, useState } from 'react';
+import { MapContext, PlacesContext } from '../context';
+import type { Feature } from '../interfaces/MapResponse';
 
 export const SearchResults = () => {
+  const [activeId, setActiveId] = useState<string | null>('');
   const { places, isLoadingPlaces } = useContext(PlacesContext);
+  const { map } = useContext(MapContext);
+
+  const onPlaceClick = (place: Feature) => {
+    const [lng, lat] = place.center;
+
+    setActiveId(place.id);
+
+    // Move the map to the selected place
+    map?.flyTo({
+      zoom: 14,
+      center: [lng, lat],
+    });
+  };
 
   if (isLoadingPlaces) {
     return <div className='alert alert-info mt-3'>Searching places...</div>;
   }
 
-  if (places.length === 0) {
+  if (!places || places.length === 0) {
     return <></>;
   }
 
   return (
     <ul className='list-group mt-3'>
       {places.map((place) => (
-        <li key={place.id} className='list-group-item list-group-item-action'>
+        <li
+          key={place.id}
+          className={`list-group-item list-group-item-action pointer ${activeId === place.id ? 'active' : ''}`}
+          onClick={() => onPlaceClick(place)}
+        >
           <h6>{place.text}</h6>
           <p
-            className='text-muted'
             style={{
               fontSize: '12px',
             }}
           >
             {place.place_name}
           </p>
-          <button className='btn btn-outline-primary btn-sm'>Get Address</button>
+          <button className={`btn btn-sm ${activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'}`}>Get Address</button>
         </li>
       ))}
     </ul>
